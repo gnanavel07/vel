0b482ce95dc11b6c7233b8b2d0973241c0334011
gnanavel07 <111863228+gnanavel07@users.noreply.github.com>
2022-08-23 15:11:46 +0530

    Add files via upload

---
+++ Myfirstclass.java
@@ -0,0 +1,19 @@
+package myproject;
+
+public class Myfirstclass {
+	
+	public void myMethod() {
+		   System.out.println("this is my method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is mymethod");
+		Myfirstclass m= new Myfirstclass();
+		m.myMethod();
+	
+	
+	}
+
+}
